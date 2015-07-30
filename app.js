5d667e30e84820a59531678e642dfdde82161622
Jamal Carvalho <jamal.a.carvalho@gmail.com>
2015-07-30 17:11:37 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,136 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+
+  res.sendFile(__dirname + '/index.html');
+
+});
+
+app.use(express.static(__dirname + '/public'));
+
+var usernames = {};
+var rooms = {'lobby' : true};
+
+var socketTable = {};
+var roomTable = {};
+
+io.on('connection', function(socket){
+
+  socket.on('add user', function(name){
+    socket.username = name;
+    socket.room = 'lobby';
+    socket.join('lobby');
+    socket.emit('chat event','hello, ' + name + '! you have entered the lobby');
+    socket.broadcast.to('lobby').emit('chat event', socket.username + " has joined the chat");
+    usernames[socket.username] = true;
+    updateUsers();
+    updateRooms();
+    updateHeader(socket);
+    console.log(socket);
+  });
+ 
+  // broadcast chat event to everyone except sender
+  socket.on('send message', function(msg){
+    socket.broadcast.to(socket.room).emit('chat message', msg,  socket.username, (new Date()).toLocaleTimeString(), "left");
+  });
+
+  socket.on('create room', function(name){
+    createRoom(name, socket);
+  });
+
+  socket.on('switch room', function(name){
+    switchRoom(name, socket);
+  });
+
+  socket.on('change user', function(name){
+    changeUser(name, socket);
+  });
+
+  // emit a disconnected message to all clients
+  socket.on('disconnect', function(){
+    socket.broadcast.to(socket.room).emit('chat event', socket.username + " has left the chat");
+    removeUser(socket);
+  });
+
+  function updateHeader(socket){
+    socket.emit('header:update', socket.room, socket.username);
+  }
+
+  function updateUsers(){
+    io.emit("update users", Object.keys(usernames).sort());
+  }
+
+  function updateRooms(){
+    io.emit("rooms:refresh", Object.keys(rooms).sort());
+  }
+
+  function removeUser(socket){
+    var name = socket.username;   
+    delete usernames[name];
+    updateUsers();
+  }
+
+  function changeUser(name, socket) {
+    name = name.toLowerCase();
+
+    if (!usernames[name] && name){      
+      removeUser(socket);
+      socket.broadcast.to(socket.room).emit('chat event', socket.username + " is now " + name);
+      socket.username = name;
+      usernames[name] = true;
+      socket.emit('update name', name);
+      updateUsers();
+      updateHeader(socket);
+    }
+    else {
+      socket.emit("alert", name + " already exists!");
+    }
+  }
+
+  function leaveRoom(socket){
+    socket.broadcast.to(socket.room).emit('chat event', socket.username + " has left the chat");
+    socket.leave(socket.room);
+    socket.room = null;
+  }
+
+  function joinRoom(name, socket){
+    if (name && rooms[name] && socket.room != name){
+      socket.room = name;
+      socket.join(name);
+      socket.broadcast.to(name).emit('chat event', socket.username + " has joined the chat");
+      socket.emit('messages:empty');
+      socket.emit('chat event', 'you are now in ' + name);
+      updateHeader(socket);
+    }
+  }
+
+  function switchRoom(name, socket){
+    if (name != socket.room){
+      leaveRoom(socket);
+      joinRoom(name, socket);
+    }
+  }
+
+  function createRoom(name, socket){
+    // all names lowercase
+    name = name.toLowerCase();
+    // if a room exists create it
+    if (!rooms[name] && name){
+      rooms[name] = true;
+      leaveRoom(socket);
+      joinRoom(name, socket);
+      updateRooms();
+    }
+    else {
+      socket.emit("alert", name + " already exists!");
+    }
+  }
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
---
+++ public/js/main.js
@@ -0,0 +1,125 @@
+/*jshint esnext: true*/
+
+  // submit a message to the server
+  $('#send').submit(sendChatMessage);
+  $("#create").submit(createNewRoom);
+  $("#change").submit(changeUserName);
+
+  var socket = io();
+  var room = "lobby";
+  var username;
+
+  $.ajax({
+    url: 'https://randomuser.me/api/',
+    dataType: 'json',
+    success: function(data){
+      username = data.results[0].user.username;
+      socket.emit('add user', username);
+    }
+  });
+
+  socket.on('header:update', updateHeader);
+
+  socket.on('messages:empty', emptyMessages);
+
+  socket.on('update name', setUser);
+
+  socket.on('rooms:refresh', refreshRooms);
+
+  socket.on('chat message', showChatMessage);
+
+  socket.on('chat event', sendChatEvent);
+
+  socket.on('update users', updateUsers);
+
+  socket.on('alert', function(msg) { alert(msg); } );
+
+  function updateHeader(room, name){
+  	$('#header').text(room + " --- Socket.io Chat --- " + name);
+  }
+
+  function sendChatMessage() {
+    var msg = $('#newChatMessage').val();
+    if (msg){
+      showChatMessage(msg, username, (new Date()).toLocaleTimeString(), "right");
+      socket.emit('send message', msg);
+      $('#newChatMessage').val('');
+    }
+    return false;
+  }
+
+  function changeUserName(){
+    var name = $("#newUserName").val();
+    if (name) {
+      $("#newUserName").val('');
+      socket.emit('change user', name);
+    }
+    return false;
+  }
+
+  function createNewRoom() {
+    var name = $("#newRoomName").val();
+    if (name) {
+      $("#newRoomName").val('');
+      socket.emit('create room', name);
+    }
+    return false;
+  }
+
+  function emptyMessages(){
+  	$('#messages').empty();
+  }
+
+  function setUser(name){
+  	username = name;
+  }
+
+  function showChatMessage(msg, user, time, align){
+	$('<li class="media text-' + align + '">' +
+	  	'<div class="media-body">' +
+	    	'<div class="media">' +
+	            '<a class="pull-' + align + '" href="#">' +
+	                '<span class="glyphicon glyphicon-user"></span>' +
+	            '</a>' +
+	            '<div class="media-body">' +
+	                msg +
+	                '<br />' +
+	               '<small class="text-muted">' + user + ' | ' + time + '</small>' +
+	                '<hr />' +
+	            '</div>' +
+	        '</div>' +
+	    '</div>' +
+	'</li>').appendTo($('#messages'));
+    $("#messages").prop({ scrollTop: $("#messages").prop("scrollHeight") });
+  }
+
+  function sendChatEvent(msg) {
+  	$('<li class="text-center"><small class="text-muted">' + msg+ '</small></li>').appendTo($('#messages'));
+  	$("#messages").prop({ scrollTop: $("#messages").prop("scrollHeight") });
+  }
+
+  function updateUsers(userlist) {
+    $('#users').empty();
+    for (var name of userlist) {
+      $('#users').append($('<li class="list-group-item">').text(name));
+    }
+  }
+
+  function refreshRooms(roomlist){
+    $('#rooms').empty();
+    for (var name of roomlist) {
+      $('<li id =' + name.toLowerCase() + ' class="list-group-item">' + name + "</li>").appendTo($('#rooms')).click(createHandler(name));
+    }
+  }
+
+  function switchRoom(name) {
+    socket.emit('switch room', name);
+  }
+
+  function createHandler(id) {
+    return function(e) {
+      switchRoom(id);
+    };
+  }
+
+
